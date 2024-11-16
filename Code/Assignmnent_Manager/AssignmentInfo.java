b802d573a9d1481f855c660eb471dcaab7b283f6
Omni-VoidTIme <paigekunze40@gmail.com>
2024-11-15 16:14:24 -0800

    add variables for AssignmentInfo and Date class

---
+++ Code/Assignmnent_Manager/AssignmentInfo.java
@@ -0,0 +1,9 @@
+public class AssignmentInfo {
+    double class_points;
+    String assignment_name;
+    Date due_date;
+    double grade_weight;
+    boolean completion;
+    String canvas_class;
+}
+
---
+++ Code/Assignmnent_Manager/Date.java
@@ -0,0 +1,5 @@
+public class Date{
+    int month;
+    int date;
+    int year;
+}
