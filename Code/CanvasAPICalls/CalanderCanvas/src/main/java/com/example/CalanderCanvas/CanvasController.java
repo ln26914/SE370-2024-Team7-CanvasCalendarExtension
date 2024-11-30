e626da04d9399b78d6e337ae529f07cad4be4f0b
spencer-purcell <0587212@my.msjc.edu>
2024-11-29 18:08:26 -0800

    Added JsonNode import to help parse strings. Created getGrades call and updated getCourses to JsonNode.

--- Code/CanvasAPICalls/CalanderCanvas/src/main/java/com/example/CalanderCanvas/CanvasController.java
+++ Code/CanvasAPICalls/CalanderCanvas/src/main/java/com/example/CalanderCanvas/CanvasController.java
@@ -1,18 +1,15 @@
 package com.example.CalanderCanvas;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @CrossOrigin
 public class CanvasController {
-
-
-    @GetMapping("/health-check")
-    public String getHealthCheck(){
-        return "Situation Normal";
-    }
 
     private final CanvasAPIService canvasService;
 
@@ -20,11 +17,18 @@ public class CanvasController {
         this.canvasService = canvasService;
     }
 
+    @GetMapping("/health-check")
+    public String getHealthCheck() {
+        return "Situation Normal";
+    }
+
     @GetMapping("/courses")
-    public String getCourses() {
+    public JsonNode getCourses() {
         return canvasService.getCourses();
     }
 
-
-
+@GetMapping("/grades")
+public List<String> getGrades() {
+    return canvasService.getGrades();
+}
 }
